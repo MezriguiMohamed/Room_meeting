d6c9fca31e32aaee190263eb7d914f34fb40677e
MezriguiMohamed <mohamedmezrigui08@gmail.com>
2024-01-23 17:25:27 +0100

    upload

---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-ZA2PFBHC.mjs";import"./chunk-PQ3BHF2K.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
+import { LoginComponent } from './login/login.component';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    StoreModule.forRoot({}, {})
   ],
   providers: [
     provideClientHydration()
